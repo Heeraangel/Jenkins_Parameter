c377d31b634e3cfb02b57210cf7392a3a44c6a87
HP <HP@HEERA>
2022-05-30 18:10:59 +0530

    Jenkins parameter

---
+++ src/test/java/parameter/JenkinsTest.java
@@ -0,0 +1,31 @@
+package parameter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinsTest{
+	@Test
+	public void parameterTest()
+	{
+		WebDriver driver = null;
+		String url=System.getProperty("URL");
+		String browser=System.getProperty("BROWSER");
+		switch(browser)
+		{
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				driver=new ChromeDriver();
+				break;
+			case "edge":
+				WebDriverManager.edgedriver().setup();
+				driver=new EdgeDriver();
+				break;
+		}
+		driver.get(url);
+		driver.quit();
+	}
+}
